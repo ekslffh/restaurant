396ce585bb92a8dd1a352cfec43ed00e308a317d
ekslffh <ekslffh@naver.com>
2022-05-03 20:54:57 +0900

    REST API 개발 4 delete, addVisit 메소드 구현

--- src/main/java/com/example/restaurant/wishlist/service/WishListService.java
+++ src/main/java/com/example/restaurant/wishlist/service/WishListService.java
@@ -4,14 +4,22 @@ import com.example.restaurant.naver.NaverClient;
 import com.example.restaurant.naver.dto.SearchImageReq;
 import com.example.restaurant.naver.dto.SearchLocalReq;
 import com.example.restaurant.wishlist.dto.WishListDto;
+import com.example.restaurant.wishlist.entity.WishListEntity;
+import com.example.restaurant.wishlist.repository.WishListRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class WishListService {
 
     private final NaverClient naverClient;
+    private final WishListRepository wishListRepository;
 
     public WishListDto search(String query) {
 
@@ -51,4 +59,61 @@ public class WishListService {
 
     }
 
+    public WishListDto add(WishListDto wishListDto) {
+        var entity = dtoToEntity(wishListDto);
+        var saveEntity = wishListRepository.save(entity);
+        return entityToDto(saveEntity);
+    }
+
+    private WishListDto entityToDto(WishListEntity wishListEntity) {
+        var dto = new WishListDto();
+        dto.setIndex(wishListEntity.getIndex());
+        dto.setTitle(wishListEntity.getTitle());
+        dto.setCategory(wishListEntity.getCategory());
+        dto.setAddress(wishListEntity.getAddress());
+        dto.setRoadAddress(wishListEntity.getRoadAddress());
+        dto.setHomePageLink(wishListEntity.getHomePageLink());
+        dto.setImageLink(wishListEntity.getImageLink());
+        dto.setVisit(wishListEntity.isVisit());
+        dto.setVisitCount(wishListEntity.getVisitCount());
+        dto.setLastVisitDate(wishListEntity.getLastVisitDate());
+        return dto;
+    }
+
+    private WishListEntity dtoToEntity(WishListDto wishListDto) {
+        var entity = new WishListEntity();
+        entity.setIndex(wishListDto.getIndex());
+        entity.setTitle(wishListDto.getTitle());
+        entity.setCategory(wishListDto.getCategory());
+        entity.setAddress(wishListDto.getAddress());
+        entity.setRoadAddress(wishListDto.getRoadAddress());
+        entity.setHomePageLink(wishListDto.getHomePageLink());
+        entity.setImageLink(wishListDto.getImageLink());
+        entity.setVisit(wishListDto.isVisit());
+        entity.setVisitCount(wishListDto.getVisitCount());
+        entity.setLastVisitDate(wishListDto.getLastVisitDate());
+        return entity;
+    }
+
+    public List<WishListDto> findAll() {
+
+        return wishListRepository
+                .ListAll()
+                .stream()
+                .map(it->entityToDto(it))
+                .collect(Collectors.toList());
+    }
+
+    public void delete(int index) {
+        wishListRepository.deleteById(index);
+    }
+
+    public void addVisit(int index) {
+        var wishItem = wishListRepository.findById(index);
+        if(wishItem.isPresent()) {
+            var item = wishItem.get();
+            item.setVisitCount(item.getVisitCount() + 1);
+        }
+
+    }
 }
